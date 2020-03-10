d099337997b9cc49cb6f6ca1442d3dabe4c7d156
lehag <lehagolovchan@yandex.ru>
2020-03-10 21:50:23 +0700

    Server update; Added Random DeckGeneration, random Trump generation, Cards from deck for players

--- src/durak/Server.java
+++ src/durak/Server.java
@@ -1,20 +1,40 @@
 package durak;
 
-import durak.game.Game;
-import durak.game.Player;
+import durak.game.*;
 
 public class Server {
 
 	public static void main(String[] args) throws InterruptedException {
 		Game game = new Game();
 
-		Player player1 = new Player();
-		Player player2 = new Player();
+		Player player1 = new Player(game);
+		Player player2 = new Player(game);
 
-		player1.register(game);
-		player2.register(game);
+		Deck deck = new Deck();
+		deck.generateDeck();
 
-		game.start();
+		System.out.println("Trump in this game:");
+		System.out.println(deck.getTrump().getSuit());
+
+		Hand hand1 = new Hand();
+		Hand hand2 = new Hand();
+
+		for (int i = 0; i < 6; i++) {
+			hand1.addCard(deck.getCurrentCardFromDeck());
+		}
+
+		for (int i = 0; i < 6; i++) {
+			hand2.addCard(deck.getCurrentCardFromDeck());
+		}
+
+		player1.handOut(hand1);
+		player2.handOut(hand2);
+
+		System.out.println("0 player cards from deck: ");
+		player1.printHand();
+
+		System.out.println("1 player cards from deck: ");
+		player2.printHand();
 
 		game.exitGame(player1.getId());
 		game.exitGame(player2.getId());
