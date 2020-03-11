d831175b7ebe0ff8238823ebf05517020356a816
Nikolay Barsukov <nb721@mail.ru>
2020-03-11 18:21:42 +0700

    Added assets

--- src/durak/game/graphics/ClientView.java
+++ src/durak/game/graphics/ClientView.java
@@ -1,16 +1,52 @@
 package durak.game.graphics;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class ClientView extends JFrame {
+
+	ClassLoader classLoader = getClass().getClassLoader();
 	private JPanel tablePanel;
+	private JPanel deckPanel;
+	private Image tableImage;
 
 	public ClientView(String windowName) {
 		super(windowName);
 		setBounds(150,150,1000,700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		try {
+			tableImage = ImageIO.read(classLoader.getResourceAsStream("assets/table.jpg"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
 		add(tablePanel);
 		setVisible(true);
+	}
+
+	private void createUIComponents() {
+		tablePanel = new JPanel() {
+
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+
+				g.drawImage(tableImage, 0, 0, tablePanel.getWidth(), tablePanel.getHeight(), null);
+			}
+		};
+
+		deckPanel = new JPanel() {
+
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+
+				DeckView dv = new DeckView("clovers__Q", classLoader);
+				dv.draw(g);
+			}
+		};
 	}
 }
--- src/durak/game/graphics/DeckView.java
+++ src/durak/game/graphics/DeckView.java
@@ -1,8 +1,28 @@
 package durak.game.graphics;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
 
 public class DeckView {
-	private Image[] faces;
-	private Image   back;
+	private Image trump;
+	private Image cover;
+
+	public DeckView(String trump, ClassLoader classLoader) {
+		String url = "assets/" + trump +".png";
+
+		try {
+			System.out.println(url);
+			this.trump = ImageIO.read(classLoader.getResourceAsStream(url));
+			cover = ImageIO.read(classLoader.getResourceAsStream("assets/cover.png"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void draw(Graphics g) {
+		System.out.println(cover.getWidth(null));
+		g.drawImage(cover, 0, 0,cover.getWidth(null), cover.getHeight(null), null);
+	}
+
 }
