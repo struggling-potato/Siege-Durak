145bde5fbf999f74823be17b43b70fff6a97e7ee
Nikolay Barsukov <nb721@mail.ru>
2020-03-12 11:42:25 +0700

    Added deck rendering, CardToImage helper

--- src/durak/Client.java
+++ src/durak/Client.java
@@ -1,34 +1,34 @@
 package durak;
 
 import durak.game.*;
+import durak.game.graphics.ClientView;
 
 public class Client {
 
     public static void main(String[] args) {
-        Game   game    = new Game();
-        Player player1 = new Player();
-        Player player2 = new Player();
-
-        player1.register(game);
-        player2.register(game);
-
-        Hand hand1 = new Hand();
-        Hand hand2 = new Hand();
-        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
-        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_J));
-        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q));
-
-        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_K));
-        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_A));
-        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_7));
-
-        player1.handOut(hand1);
-        player2.handOut(hand2);
-
-        player1.printHand();
-        player2.printHand();
-
-        game.exitGame(player1.getId());
-        game.exitGame(player2.getId());
+        ClientView clientView = new ClientView("Siege-Durak");
+        clientView.setTrump(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
+//        Game   game    = new Game();
+//        Player player1 = new Player(game);
+//        Player player2 = new Player(game);
+//
+//        Hand hand1 = new Hand();
+//        Hand hand2 = new Hand();
+//        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
+//        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_J));
+//        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q));
+//
+//        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_K));
+//        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_A));
+//        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_7));
+//
+//        player1.handOut(hand1);
+//        player2.handOut(hand2);
+//
+//        player1.printHand();
+//        player2.printHand();
+//
+//        game.exitGame(player1.getId());
+//        game.exitGame(player2.getId());
     }
 }
