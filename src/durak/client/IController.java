87dcb39d1c229e3893e9ea0ec358d5c19cc9c938
VetchinnikovaA <51243920+VetchinnikovaA@users.noreply.github.com>
2020-03-16 19:17:00 +0700

    Update IController.java

--- src/durak/client/IController.java
+++ src/durak/client/IController.java
@@ -3,6 +3,8 @@ package durak.client;
 public interface IController {
 
 	void onButtonPressed(int buttonId);
+	
+	void onCardClicked(int pairIdx, boolean mode);
 
 	void onTableClicked(int pairIdx, boolean mode);
 }
