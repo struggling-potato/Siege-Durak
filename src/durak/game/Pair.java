f121b50ae2c7a9aa780b36f8434cd9cb8333dc75
bitLight <vitaliy.yakovenko.nstu@yandex.ru>
2020-03-18 21:32:08 +0700

    Game classes improvement

--- src/durak/game/Pair.java
+++ src/durak/game/Pair.java
@@ -16,6 +16,10 @@ public class Pair implements Serializable {
 		bot = card;
 	}
 
+	public Pair(Card card) {
+		bot = card;
+	}
+
 	public ArrayList<Card> getCards() {
 		ArrayList<Card> pair = new ArrayList<>();
 		pair.add(bot);
