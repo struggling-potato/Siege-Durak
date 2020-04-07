dd28f8e58951594292684fe4d23035ad3345b793
VetchinnikovaA <51243920+VetchinnikovaA@users.noreply.github.com>
2020-04-07 21:55:14 +0700

    new function in IGame

--- src/durak/game/IGame.java
+++ src/durak/game/IGame.java
@@ -1,10 +1,16 @@
 package durak.game;
 
+import java.util.List;
+
 public interface IGame {
 
 	void throwCard(int playerId, Card card); //походить
 
+	void throwCards(int playerId, List<Card> cards); //походить
+
 	void tossCard(int playerId, Card card); //подбросить
+
+	void tossCards(int playerId, List<Card> cards); //подбросить
 
 	void beatCard(int playerId, Pair pair); //отбить
 
