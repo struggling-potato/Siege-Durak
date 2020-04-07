62eb31f65c9e87731f2534230c2565835ee04ebf
Barsukasm <nb721@mail.ru>
2020-04-07 11:56:29 +0700

    UI ready to integration

--- src/durak/Client.java
+++ src/durak/Client.java
@@ -9,54 +9,45 @@ import java.util.ArrayList;
 public class Client {
 
     public static void main(String[] args) {
-        ClientView clientView = new ClientView();
-        clientView.setTrump(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
-        clientView.drawStringState("Новый стейт");
-        Hand hand1 = new Hand();
-        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
-        hand1.addCard(new Card(Suit.SUIT_CLOVERS, Rank.RANK_J));
-        hand1.addCard(new Card(Suit.SUIT_PIKES, Rank.RANK_Q));
-        clientView.drawHandOut(hand1);
-        clientView.setButtonState(Buttons.BUTTON_GIVEUP, true);
-//        Game   game    = new Game();
-        Player player1 = new Player();
-        Player player2 = new Player();
+	    /*ClientView clientView = new ClientView();
+	    clientView.setTrump(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
+	    clientView.drawStringState("Новый стейт");
+	    Hand hand1 = new Hand();
+	    hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
+	    hand1.addCard(new Card(Suit.SUIT_CLOVERS, Rank.RANK_J));
+	    hand1.addCard(new Card(Suit.SUIT_PIKES, Rank.RANK_Q));
+	    clientView.drawHandOut(hand1);
+	    clientView.setButtonState(Buttons.BUTTON_GIVEUP, true);
+	    Player player1 = new Player();
+	    Player player2 = new Player();
 
-//
-        Hand hand2 = new Hand();
-        Hand hand3 = new Hand();
-        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
-        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_J));
-        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q));
+	    Hand hand2 = new Hand();
+	    Hand hand3 = new Hand();
+	    hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
+	    hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_J));
+	    hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q));
 
-        hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_K));
-        hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_A));
-        hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_7));
-        hand3.addCard(new Card(Suit.SUIT_PIKES, Rank.RANK_Q));
+	    hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_K));
+	    hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_A));
+	    hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_7));
+	    hand3.addCard(new Card(Suit.SUIT_PIKES, Rank.RANK_Q));
 
-        player1.handOut(hand2);
-        player2.handOut(hand3);
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(player1);
-        players.add(player2);
-        clientView.drawPlayers(players);
-        Deck deck = new Deck();
-        hand1.addCard(deck.takeCardFromDeck());
+	    player1.handOut(hand2);
+	    player2.handOut(hand3);
+	    ArrayList<Player> players = new ArrayList<>();
+	    players.add(player1);
+	    players.add(player2);
+	    clientView.drawPlayers(players);
+	    Deck deck = new Deck();
+	    hand1.addCard(deck.takeCardFromDeck());
 	    hand1.addCard(deck.takeCardFromDeck());
 	    hand1.addCard(deck.takeCardFromDeck());
 	    clientView.drawHandOut(hand1);
-        Table table = new Table(deck);
-        ArrayList<Pair> thrownCards = new ArrayList<>();
-        thrownCards.add(new Pair(new Card(Suit.SUIT_HEARTS, Rank.RANK_K), new Card(Suit.SUIT_PIKES, Rank.RANK_Q)));
+	    Table           table       = new Table(deck);
+	    ArrayList<Pair> thrownCards = new ArrayList<>();
+	    thrownCards.add(new Pair(new Card(Suit.SUIT_HEARTS, Rank.RANK_K), new Card(Suit.SUIT_PIKES, Rank.RANK_Q)));
 	    thrownCards.add(new Pair(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q), new Card(Suit.SUIT_PIKES, Rank.RANK_A)));
-        table.setThrownCards(thrownCards);
-        clientView.drawTable(table);
-
-//
-//        player1.printHand();
-//        player2.printHand();
-//
-//        game.exitGame(player1.getId());
-//        game.exitGame(player2.getId());
+	    table.setThrownCards(thrownCards);  // Если хотите глянуть, то добавьте сеттер в класс Table или придумайте другой способ сформировать пары
+	    clientView.drawTable(table);*/
     }
 }
