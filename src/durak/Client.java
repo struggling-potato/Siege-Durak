c19222567c63e61abde7930900219f344063a2e0
Barsukasm <nb721@mail.ru>
2020-04-07 11:44:37 +0700

    rebase before push to repo

--- src/durak/Client.java
+++ src/durak/Client.java
@@ -1,35 +1,57 @@
 package durak;
 
 import durak.game.*;
+import durak.game.graphics.Buttons;
 import durak.game.graphics.ClientView;
+
+import java.util.ArrayList;
 
 public class Client {
 
     public static void main(String[] args) {
-        ClientView clientView = new ClientView("Siege-Durak");
+        ClientView clientView = new ClientView();
         clientView.setTrump(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
         clientView.drawStringState("Новый стейт");
         Hand hand1 = new Hand();
         hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
-        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_J));
-        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q));
+        hand1.addCard(new Card(Suit.SUIT_CLOVERS, Rank.RANK_J));
+        hand1.addCard(new Card(Suit.SUIT_PIKES, Rank.RANK_Q));
         clientView.drawHandOut(hand1);
+        clientView.setButtonState(Buttons.BUTTON_GIVEUP, true);
 //        Game   game    = new Game();
-//        Player player1 = new Player(game);
-//        Player player2 = new Player(game);
+        Player player1 = new Player();
+        Player player2 = new Player();
+
 //
-//        Hand hand1 = new Hand();
-//        Hand hand2 = new Hand();
-//        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
-//        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_J));
-//        hand1.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q));
-//
-//        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_K));
-//        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_A));
-//        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_7));
-//
-//        player1.handOut(hand1);
-//        player2.handOut(hand2);
+        Hand hand2 = new Hand();
+        Hand hand3 = new Hand();
+        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_6));
+        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_J));
+        hand2.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q));
+
+        hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_K));
+        hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_A));
+        hand3.addCard(new Card(Suit.SUIT_HEARTS, Rank.RANK_7));
+        hand3.addCard(new Card(Suit.SUIT_PIKES, Rank.RANK_Q));
+
+        player1.handOut(hand2);
+        player2.handOut(hand3);
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        clientView.drawPlayers(players);
+        Deck deck = new Deck();
+        hand1.addCard(deck.takeCardFromDeck());
+	    hand1.addCard(deck.takeCardFromDeck());
+	    hand1.addCard(deck.takeCardFromDeck());
+	    clientView.drawHandOut(hand1);
+        Table table = new Table(deck);
+        ArrayList<Pair> thrownCards = new ArrayList<>();
+        thrownCards.add(new Pair(new Card(Suit.SUIT_HEARTS, Rank.RANK_K), new Card(Suit.SUIT_PIKES, Rank.RANK_Q)));
+	    thrownCards.add(new Pair(new Card(Suit.SUIT_HEARTS, Rank.RANK_Q), new Card(Suit.SUIT_PIKES, Rank.RANK_A)));
+        table.setThrownCards(thrownCards);
+        clientView.drawTable(table);
+
 //
 //        player1.printHand();
 //        player2.printHand();
--- src/durak/game/graphics/ClientView.java
+++ src/durak/game/graphics/ClientView.java
@@ -1,11 +1,8 @@
 package durak.game.graphics;
 
 import durak.client.IView;
-import durak.game.Card;
+import durak.game.*;
 
-import durak.game.Hand;
-import durak.game.Player;
-import durak.game.Table;
 import durak.game.graphics.utils.CardToImage;
 
 import javax.imageio.ImageIO;
@@ -14,8 +11,17 @@ import java.awt.*;
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ClientView extends JFrame implements IView {
+
+	private final static HashMap<Buttons, String> BUTTON_MAP = new HashMap<>();
+	static {
+		BUTTON_MAP.put(Buttons.BUTTON_START, "Начать игру");
+		BUTTON_MAP.put(Buttons.BUTTON_PASS, "Закончить ход");
+		BUTTON_MAP.put(Buttons.BUTTON_GIVEUP, "Взять карты");
+		BUTTON_MAP.put(Buttons.BUTTONS_EXITGAME, "Выйти из игры");
+	}
 
 	private ClassLoader classLoader = getClass().getClassLoader();
 	private JPanel      tablePanel;
@@ -29,15 +35,15 @@ public class ClientView extends JFrame implements IView {
 	private JPanel      enemyPlayer2Panel;
 	private JPanel      gameStatusBar;
 	private JLabel      gameStateLabel;
+	private JPanel pairsPanel;
 	private Image       tableImage;
 	private DeckView    deckView = new DeckView();
-	private HandView    playerHandView = new HandView();
 	private Hand        playerHand;
 	private CardPanel[] cardPanels;
 	private boolean     cardsState = false;
 
-	public ClientView(String windowName) {
-		super(windowName);
+	public ClientView() {
+		super("Siege-Durak");
 		setBounds(50,0,1600,1050);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -53,19 +59,26 @@ public class ClientView extends JFrame implements IView {
 	}
 
 	public void setTrump(Card card) {
-
-//		System.out.println("clientView.setTrump called");
 		deckView.setTrump(card, classLoader);
 		repaint();
 	}
-//
-//	public void drawPlayerHand(Hand hand, int pos) {
-//
-//	}
+
 
 	@Override
 	public void drawTable(Table table) {
+		Suit trumpSuit = table.getDeck().getTrump().getSuit();
+		setTrump(new Card(trumpSuit, Rank.RANK_A));
 
+		for(Pair pair: table.getThrownCard()) {
+			PairPanel pairPanel = new PairPanel(pair);
+			pairPanel.setMinimumSize(new Dimension(172, 330));
+			pairPanel.setPreferredSize(new Dimension(172, 330));
+			pairPanel.setOpaque(false);
+			pairsPanel.add(pairPanel);
+			pairPanel.revalidate();
+			validate();
+		}
+		repaint();
 	}
 
 	@Override
@@ -76,35 +89,84 @@ public class ClientView extends JFrame implements IView {
 	@Override
 	public void drawHandOut(Hand hand) {
 		playerHand = hand;
+		if (playerHand == null) return;
+		playerHandPanel.removeAll();
+		ArrayList<Card> cards = playerHand.getCards();
+		String url;
+
+		for (int i = 0; i < cards.size(); i++) {
+			url = CardToImage.getCartImageUrl(cards.get(i));
+
+			CardPanel cardPanel = new CardPanel(url);
+			cardPanel.setMinimumSize(new Dimension(172, 270));
+			cardPanel.setPreferredSize(new Dimension(172, 270));
+			cardPanel.setOpaque(false);
+			playerHandPanel.add(cardPanel);
+			playerHandPanel.revalidate();
+			validate();
+		}
 		repaint();
 	}
 
 	@Override
 	public void drawPlayers(ArrayList<Player> players) {
+		if (players == null) return;
+		Player player1 = null;
+		Player player2 = null;
+		try {
+			player1 = players.get(0);
+			player2 = players.get(1);
+		} catch (IndexOutOfBoundsException ex) {
+			System.out.println(ex.getMessage());
+		}
+		String url = "assets/cover.png";
 
+		enemyPlayer1Panel.removeAll();
+		enemyPlayer2Panel.removeAll();
+
+		if (player1 != null) {
+			for (int i = 0; i < player1.getHandSize(); i++) {
+				CardPanel cardPanel = new CardPanel(url);
+				cardPanel.setState(false);
+				cardPanel.setSelected(true);
+				cardPanel.setMinimumSize(new Dimension(172, 260));
+				cardPanel.setPreferredSize(new Dimension(172, 260));
+				cardPanel.setOpaque(false);
+				enemyPlayer1Panel.add(cardPanel);
+				enemyPlayer1Panel.revalidate();
+				validate();
+			}
+		}
+		if (player2 != null) {
+			for (int i = 0; i < player2.getHandSize(); i++) {
+				CardPanel cardPanel = new CardPanel(url);
+				cardPanel.setState(false);
+				cardPanel.setSelected(true);
+				cardPanel.setMinimumSize(new Dimension(172, 260));
+				cardPanel.setPreferredSize(new Dimension(172, 260));
+				cardPanel.setOpaque(false);
+				enemyPlayer2Panel.add(cardPanel);
+				enemyPlayer2Panel.revalidate();
+				validate();
+			}
+		}
+		repaint();
 	}
 
 	@Override
 	public void setButtonState(Buttons buttonId, boolean visible) {
-
+		actionButton.setText(BUTTON_MAP.get(buttonId));
+		actionButton.setVisible(visible);
 	}
 
 	@Override
 	public void setCardsState(boolean clickable) {
-
+		for (CardPanel cardPanel: cardPanels) {
+			cardPanel.setState(clickable);
+		}
 	}
 
 	private void createUIComponents() {
-		tablePanel = new JPanel() {
-
-			@Override
-			protected void paintComponent(Graphics g) {
-				super.paintComponent(g);
-
-				g.drawImage(tableImage, 0, 0, tablePanel.getWidth(), tablePanel.getHeight(), null);
-			}
-		};
-
 		deckPanel = new JPanel() {
 
 			@Override
@@ -122,36 +184,36 @@ public class ClientView extends JFrame implements IView {
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
+			}
+		};
 
-				if (playerHand == null) return;
-				playerHandPanel.removeAll();
-				ArrayList<Card> cards = playerHand.getCards();
-//				cardPanels = new JPanel[cards.size()];
-				String url;
-				int offsetX = 0;
+		tablePanel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
 
-				for (int i = 0; i < cards.size(); i++) {
-					url = CardToImage.getCartImageUrl(cards.get(i));
+				g.drawImage(tableImage, 0, 0, tablePanel.getWidth(), tablePanel.getHeight(), null);
+			}
+		};
 
-//					handPanels[i].setMinimumSize(new Dimension(172, 260));
-//					handPanels[i].setPreferredSize(new Dimension(172, 260));
-//					handPanels[i].setOpaque(false);
-//					playerHandPanel.add(handPanels[i]);
-//					g.drawImage(cardImage, offsetX, 10, null);
-					offsetX += 175;
-//					playerHandPanel.add(new JButton() {
-//						@Override
-//						protected void paintComponent(Graphics g) {
-//							super.paintComponent(g);
-//
-//							this.setMinimumSize(new Dimension(172, 260));
-//							g.drawImage(cardImage, 0, 10, null);
-//						}
-//					});
-					playerHandPanel.revalidate();
-					validate();
-				}
+		enemyPlayer1Panel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+			}
+		};
 
+		enemyPlayer2Panel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+			}
+		};
+
+		pairsPanel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
 			}
 		};
 	}
